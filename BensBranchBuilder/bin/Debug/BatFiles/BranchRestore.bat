c23f9290779a45d77beba26f3a1efff95ff9373a
sol3uk <ben@sol3.me>
2019-04-23 10:30:58 +0100

    Ignore Bin

--- BensBranchBuilder/bin/Debug/BatFiles/BranchRestore.bat
+++
@@ -1,6 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-echo cd C:\JL\TMS\PreStaging
-cd C:\JL\TMS\PreStaging
-echo..\NuGet.exe restore .\web\JobLogic.Published.sln
-..\NuGet.exe restore .\web\JobLogic.Published.sln
-pause
